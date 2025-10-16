c4c8526b444c1654188f974dbff307a5a716474f
pavanironanki <pavanironanki55@gmail.com>
2025-10-15 22:21:34 -0500

    text updates

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,5 @@
 export const contactsData = {
     email: 'pavanironanki55@gmail.com',
-    phone: '+15159163519',
     address: 'Ames, Iowa, United States ',
      github: 'https://github.com/pavanironanki',
   linkedIn: 'https://www.linkedin.com/in/pavani-ronanki-7229b4214/',
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,11 +4,9 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is Pavani Ronanki. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'pavanironanki55@gmail.com',
-  phone: '+5159163519',
   address: 'Ames, Iowa, United States ',
   github: 'https://github.com/pavanironanki',
   linkedIn: 'https://www.linkedin.com/in/pavani-ronanki-7229b4214/',
   leetcode: "https://leetcode.com/pavanironanki/",
   devUsername: "pavani.ronanki",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
 } 
